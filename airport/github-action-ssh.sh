7b252d97d26569708b821a8f7f3933138f688f63
yanbo <yanbo2695388808@gmail.com>
2023-03-29 15:54:55 +0800

    some adjust

--- airport/github-action-ssh.sh
+++ airport/github-action-ssh.sh
@@ -13,14 +13,16 @@ fi
 echo "### Update user: $USER password ###"
 echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
 
-log=./airport/.log
 address=./airport/address
+log=./airport/.log
+container_name=deploy_container
 
-rm -f $log
-./airport/ngrok authtoken "$TOKEN"
-./airport/ngrok tcp 22 --log "$log" &
+docker pull yanboer/deploy-environment:alpine > /dev/null
+docker run -itd --name $container_name --net=host yanboer/deploy-environment:alpine --authtoken $TOKEN --region us --log stdout tcp 22 > /dev/null
 
 sleep 10
+docker logs $container_name > $log
+
 HAS_ERRORS=$(grep "command failed" < $log)
 
 if [[ -z "$HAS_ERRORS" ]]; then
@@ -35,3 +37,4 @@ else
   echo "$HAS_ERRORS"
   exit 4
 fi
+
